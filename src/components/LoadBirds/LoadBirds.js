8ae5228c1292047b03c1b6cafb5c4ef1875c5c60
Sajalhowlader <sajolhowlader08@gmail.com>
2022-03-26 18:02:32 +0600

    add props

--- src/components/LoadBirds/LoadBirds.js
+++ src/components/LoadBirds/LoadBirds.js
@@ -90,16 +90,30 @@ const LoadBirds = () => {
             </div>
             <div className='question'>
                 <h1 className='text-center mb-4'>Props Vs State</h1>
+                <div className=' d-flex m-3 '>
+                    <div>
+                        <strong >
+                            <h3 >Props</h3>
+                            01. Props are read-only <br />
+                            02. Props are immutable <br />
+                            03. Props can be accessed by child components.<br />
+                            04. Stateless component can have Props<br />
+                            05. Props make components reusable.<br />
+                            06.Props are used to communicate between components
+                        </strong>
+                    </div>
+                    <div>
+                        <strong>
+                            <h3>State</h3>
+                            01. State change can be asynchronous.<br />
+                            02. State is mutable.<br />
+                            03. State cannot be accessed by child Components.<br />
+                            04. Stateless components cannot have State.<br />
+                            05. State cannot make components by reusable.<br />
+                            06. States can be used for rendering dynamic changes with the components</strong>
+                    </div>
+                </div>
 
-                <strong>
-
-                    01. Props are read-only
-                    02. Props are immutable
-                    03. Props can be accessed by child components.
-                    04. Stateless component can have Props
-                    05. Props make components reusable.
-                    06.Props are used to communicate between components
-                </strong>
             </div>
 
         </div>
